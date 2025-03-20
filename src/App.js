da4c05cd4b6fb9e649a3219a616fbe1bd626e151
FABRE-Alexandre-2326043 <alexandre.fabre.2@etu.univ-amu.fr>
2025-03-20 09:02:33 +0100

    afficher les tâches

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data.json';
+function findProgressing(tache) {
+    
+  }
 
 function App() {
+  var listTaches = data["taches"].map(tache => <li key = {tache["id"]}> {tache["title"]}</li>);
+  var nbProgessing = 0;
+  data["taches"].forEach(function(tache){
+    if (!tache["done"]){
+      ++nbProgessing;
+    }
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1> To Do amU </h1>
+        <h2> {data["taches"].length} tâches, dont {nbProgessing} en cours </h2>
+        <ul>
+          {listTaches}
+        </ul>
       </header>
+
+      <footer>
+        <a>Tâche </a>
+        <a>Catégorie</a>
+      </footer>
     </div>
   );
 }
