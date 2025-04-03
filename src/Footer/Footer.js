3020ddd85ac248e3540d888e23b2e76eeca3565a
FABRE-Alexandre-2326043 <alexandre.fabre.2@etu.univ-amu.fr>
2025-04-03 11:49:11 +0200

    nettoyage du footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,15 +1,9 @@
-import { useContext } from 'react';
-import { useState } from 'react';
-import { TodoContext } from '../TodoContext/TodoContext';
 import { FormTask } from '../FormTask/FormTask';
 import { CategorieForm } from '../CategorieForm/CategorieForm'
 
 
 
 function Footer(){
-    const [text2, setText2] = useState('');
-    const {currentTodo, setCurrentTodo} = useContext(TodoContext);
-
     return(
         <footer>
             <FormTask/><br/>               
